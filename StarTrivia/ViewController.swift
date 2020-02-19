ba75eeba9b86ef6ab6902084a0189a5be1a543a9
Shwetansh Srivastava <shwetansh.srivastava@gmail.com>
2020-02-19 23:15:15 +0530

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Shwetansh Srivastava on 19/02/20.
+//  Copyright © 2020 Shwetansh Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
